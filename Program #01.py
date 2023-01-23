33e2bdaae4cc6a44e6f2744cb6d270fd06b87fdd
Muhammad-Rebaal <122050424+Muhammad-Rebaal@users.noreply.github.com>
2023-01-23 10:39:31 -0800

    Add files via upload

---
+++ Program #01.py
@@ -0,0 +1,18 @@
+# Program to add two numbers
+num_1=int(input("first number?"))
+num_2=int(input("second number?"))
+num_3=int(input("third number?"))
+num_4=int(input("fourth number?"))
+
+def sum(a,b):
+    return a+b
+    
+print(sum(num_1,num_2))
+    
+
+
+def multi(c,d):
+    return c*d
+    
+print(multi(num_3,num_4))
+    
